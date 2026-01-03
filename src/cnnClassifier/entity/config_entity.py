3de7b1018ada63284a8fea0ebd6f58de0af4349f
Supratim Saha <supratimsmail@gmail.com>
2026-01-04 01:09:23 +0530

    Adding dvc.yaml

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -10,6 +10,7 @@ class DataIngestionConfig:
     unzip_dir: Path
 
 
+
 @dataclass(frozen=True)
 class PrepareBaseModelConfig:
     root_dir: Path
@@ -22,6 +23,7 @@ class PrepareBaseModelConfig:
     params_classes: int
 
 
+
 @dataclass(frozen=True)
 class TrainingConfig:
     root_dir: Path
@@ -32,3 +34,13 @@ class TrainingConfig:
     params_batch_size: int
     params_is_augmentation: bool
     params_image_size: list
+
+
+@dataclass(frozen=True)
+class EvaluationConfig:
+    path_of_model: Path
+    training_data: Path
+    all_params: dict
+    mlflow_uri: str
+    params_image_size: list
+    params_batch_size: int
